c1e6627b300e6e1a4d04d574cfe5aca7eb83c5d5
Jean <jean@Jeans-MacBook-Pro.local>
2025-04-01 10:39:41 +0100

    Handle gh-pages base dir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
+  base: '/three_starter',
   server: { port: 3000 },
   plugins: [injectHTML()],
 });
